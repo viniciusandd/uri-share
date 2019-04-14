df533dbb2a5f8e1c464b93596bb5af5f957f5211
viniciusandd <viniciusemanoelandrade@hotmail.com>
2019-04-14 20:31:53 -0300

    Ajustes no ambiente (arquivo de configuracoes) | Criacao de novas tabelas (classes) | Criacao do banco de dados | Inicio do desenvolvimento dos templates (Index e Base)

--- app/controllers/home.py
+++ app/controllers/home.py
@@ -1,5 +1,11 @@
+from flask import render_template
 from app import app
 
 @app.route("/")
-def hello():
-    return "Hello"
+def raiz():
+    return "Bem vindo ao Share!"
+
+@app.route("/index", defaults={"usuario":None})
+@app.route("/index/<usuario>")
+def index(usuario):
+    return render_template('index.html', usuario=usuario)
--- app/models/tables.py
+++ app/models/tables.py
@@ -13,48 +13,57 @@ class Perfil(db.Model):
     bairro        = db.Column(db.String)
     cep           = db.Column(db.String(8))
     logo          = db.Column(db.BLOB)
-
-    def __init__(
-        self, 
-        razao_social,
-        nome_fantasia, 
-        cnpj,
-        senha, 
-        logradouro,
-        complemento,
-        numero,
-        bairro,
-        cep        
-    ):
-    self.razao_social = razao_social
-    self.nome_fantasia = nome_fantasia
-    self.cnpj = cnpj
-    self.senha = senha
-    self.logradouro = logradouro
-    self.complemento = complemento
-    self.numero = numero
-    self.bairro = bairro
-    self.cep = cep
+    cidade_id     = db.Column(db.Integer, db.ForeignKey('cidades.id'))
+    estado_id     = db.Column(db.Integer, db.ForeignKey('estados.id'))
+    pais_id       = db.Column(db.Integer, db.ForeignKey('paises.id'))
+    cidade        = db.relationship('Cidade', foreign_keys=cidade_id)
+    estado        = db.relationship('Estado', foreign_keys=estado_id)
+    pais          = db.relationship('Pais', foreign_keys=pais_id)
 
     def __repr__(self):
         return "<Perfil %r>" % self.razao_social
+
+class Cidade(db.Model):
+    __tablename__ = "cidades"
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String)    
+
+class Estado(db.Model):
+    __tablename__ = "estados"
+    id    = db.Column(db.Integer, primary_key=True)
+    nome  = db.Column(db.String)
+    sigla = db.Column(db.String(2), unique=True)
 
 class Postagem(db.Model):
     __tablename__   = "postagens"
     id              = db.Column(db.Integer, primary_key=True)
     perfil_id       = db.Column(db.Integer, db.ForeignKey('perfis.id'))
+    categoria_id    = db.Column(db.Integer, db.ForeignKey('categorias.id'))
     conteudo        = db.Column(db.Text)
     data            = db.Column(db.Date)
     hora            = db.Column(db.Time)
     media_avaliacao = db.Column(db.Float)
     perfil          = db.relationship('Perfil', foreign_keys=perfil_id)
-
-    def __init__(self, perfil_id, conteudo, data, hora, media_avaliacao):
-        self.perfil_id = perfil_id
-        self.conteudo = conteudo
-        self.data = data
-        self.hora = hora
-        self.media_avaliacao = media_avaliacao
+    categoria       = db.relationship('Categoria', foreign_keys=categoria_id)
 
     def __repr__(self):
-        retun "<Postagem %r>" % self.id        
+        return "<Postagem %r>" % self.id
+
+class Categoria(db.Model):
+    __tablename__ = "categorias"
+    id = db.Column(db.Integer, primary_key=True)
+    descricao = db.Column(db.String)
+
+class Comentario(db.Model):
+    __tablename__ = "comentarios"
+    id          = db.Column(db.Integer, primary_key=True)
+    perfil_id   = db.Column(db.Integer)
+    postagem_id = db.Column(db.Integer)
+    conteudo    = db.Column(db.String)
+    data        = db.Column(db.Date)
+    hora        = db.Column(db.Time)
+    perfil      = db.relationship('Perfil', foreign_keys=perfil_id)
+    postagem    = db.relationship('Postagem', foreign_keys=postagem_id)
+
+    def __repr__(self):
+        return "<Comentario %r>" % self.id
