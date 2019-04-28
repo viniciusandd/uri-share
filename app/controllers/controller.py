47be801617daf945020d4e3c91934cf05923f087
viniciusandd <viniciusemanoelandrade@hotmail.com>
2019-04-27 23:24:12 -0300

    Criando classe para o formulário de registrar + Criação do template

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -13,26 +13,27 @@ def load_user(id):
 def raiz():
     return "Bem vindo ao Share!"
 
-@app.route("/home")
-def index():
-    return render_template('home.html')
+@app.route("/registrar")
+def registrar():
+    formulario = PerfilForm()
+    return render_template('registrar.html', formulario=formulario)
 
-@app.route("/cadastrar_perfil")
-def cadastrar_perfil():
-    return 'Cadastrar perfil'
-
+# Para que o login funcione, é necessário que o campo formulario.csrf_token
+# esteja no HTML, sem isso ele não será um formulário válido.
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     formulario = LoginForm()
     if formulario.validate_on_submit():
-        perfil = Perfil.query.filter_by(cnpj=formulario.usuario.data).first()
+        perfil = Perfil.query.filter_by(cnpj=formulario.cnpj.data).first()        
         if perfil and perfil.senha == formulario.senha.data:
             login_user(perfil)
             flash("Login efetuado com sucesso.")
             return redirect(url_for("home"))
         else:
             flash("Credenciais inválidas.")
-
+    else:
+        print(formulario.errors)
+        
     return render_template('login.html', formulario=formulario)
 
 @app.route("/logout")
@@ -40,3 +41,7 @@ def logout():
     logout_user()
     flash("Logout efetuado com sucesso.")
     return redirect(url_for("index"))
+
+@app.route("/home")
+def index():
+    return render_template('home.html')
--- app/models/form.py
+++ app/models/form.py
@@ -1,8 +1,22 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField
+from wtforms import StringField, PasswordField, BooleanField, FileField, SelectField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    usuario = StringField("usuario", validators=[DataRequired()])
-    senha = PasswordField("senha", validators=[DataRequired()])
+    cnpj          = StringField("cnpj", validators=[DataRequired()])
+    senha         = PasswordField("senha", validators=[DataRequired()])
     lembrar_senha = BooleanField("lembrar_senha")
+
+class PerfilForm(FlaskForm):
+    razao_social    = StringField("razao_social", validators=[DataRequired])
+    nome_fantasia   = StringField("nome_fantasia", validators=[DataRequired])
+    cnpj            = StringField("cnpj", validators=[DataRequired])
+    senha           = PasswordField("senha", validators=[DataRequired])
+    senha_confirmar = PasswordField("senha_confirmar", validators=[DataRequired])
+    logradouro      = StringField("logradouro", validators=[DataRequired])
+    complemento     = StringField("complemento")
+    numero          = StringField("numero", validators=[DataRequired])
+    bairro          = StringField("bairro", validators=[DataRequired])
+    cep             = StringField("cep", validators=[DataRequired])
+    logo            = FileField("logo")
+    cidade          = SelectField("cidade")
